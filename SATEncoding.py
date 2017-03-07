4b4adf725e5c0897ba3338e6c899fbf45c932d46
shreyasvgr <shreyasg@usc.edu>
2017-03-06 19:19:51 -0800

    Dpll executed succesfully with all test cases executed

--- SATEncoding.py
+++ SATEncoding.py
@@ -1,5 +1,5 @@
-from sets import Set
 from copy import deepcopy
+import copy
 
 input = open("Test_Cases/input5.txt",'r')
 output = open("Test_Cases/output.txt",'w')
@@ -7,29 +7,20 @@ output = open("Test_Cases/output.txt",'w')
 friends = []
 enemies = []
 list_clauses = []
-symbols = Set([])
+symbols = set()
+file_logs = ""
 global guests
 global tables
-
-
-def Answer(model):
-    print model
-    result = []
-    for item in model:
-        if item[0] != "~":
-            result.append(item)
-            print item
-    print result
 
 def Cond1():
     for i in xrange(1,guests+1):
         clause = []
         alt_clause = []
         for j in xrange(1,tables+1):
-            clause.append("~X"+str(i)+str(j))
-            alt_clause.append("X"+str(i)+str(j))
+            clause.append("~X" + "-" + str(i) + "-" + str(j))
+            alt_clause.append("X"+ "-" + str(i)+ "-" +str(j))
             #symbols.add("~X"+str(i)+str(j))
-            symbols.add("X"+str(i)+str(j))
+            symbols.add("X"+ "-" + str(i) + "-" + str(j))
         list_clauses.append(alt_clause)
         if tables > 1:
             list_clauses.append(clause)
@@ -40,11 +31,11 @@ def Cond2():
         for i in xrange(1,tables+1):
             clause = []
             alt_clause = []
-            clause.extend(["~X"+str(a)+str(i),"X"+str(b)+str(i)])
-            alt_clause.extend(["X"+str(a)+str(i),"~X"+str(b)+str(i)])
+            clause.extend(["~X"+ "-" +str(a)+ "-" +str(i),"X" + "-" + str(b) + "-" +str(i)])
+            alt_clause.extend(["X"+ "-" + str(a) + "-" +str(i),"~X"+ "-"+ str(b)+"-"+ str(i)])
             #symbols.add("~X"+str(a)+str(i))
-            symbols.add("X"+str(b)+str(i))
-            symbols.add("X"+str(a)+str(i))
+            symbols.add("X"+ "-" +str(b)+ "-" +str(i))
+            symbols.add("X"+ "-" +str(a)+ "-" +str(i))
             #symbols.add("~X"+str(b)+str(i))
             list_clauses.append(clause)
             list_clauses.append(alt_clause)
@@ -54,106 +45,130 @@ def Cond3():
         a,b = e
         for i in xrange(1,tables+1):
             clause = []
-            clause.extend(["~X"+str(a)+str(i),"~X"+str(b)+str(i)])
+            clause.extend(["~X"+ "-" +str(a)+ "-"+str(i),"~X"+ "-"+ str(b)+ "-" +str(i)])
             #symbols.add("~X"+str(a)+str(i))
             #symbols.add("~X"+str(b)+str(i))
             list_clauses.append(clause)
 
-def ModelCheck(newClause,model):
-    #newClause = deepcopy(old_clause)
-    remove_clauses = []
+def remove(symbols,P):
+    temp = set()
+    if P[0] == "~":
+        symbols.discard(P[1:])
+    else:
+        symbols.discard(P)
+        for s in symbols:
+            if s[0:3] == P[0:3]:
+                temp.add(s)
+        for i in temp:
+            symbols.discard(i)
+
+def Union(model,P,type):
+    if type:
+        model.add(P)
+    else:
+        model.add("~"+P)
+    return copy.deepcopy(model)
+
+def Answer(model):
+    global guests
+    global file_logs
+    for i in xrange(guests):
+        for j in model:
+            element = str(j).split("-")
+            if element[0] == "X" and element[1] == str(i+1):
+                file_logs += "\n" + element[1] + " " + element[2]
+
+def ModelCheck(clauses, model):
+    #clauses = deepcopy(old_clause)
     if not model:
         return "None"
-    for mod_ele in model:
-        for clause_ele in newClause:
-            if not clause_ele:
+
+    remove_clauses = []
+    for mod_element in model:
+        for clause_element in clauses:
+            if not clause_element:
                 return False
-            if mod_ele in clause_ele:
-                remove_clauses.append(clause_ele)
-                #newClause.remove(clause_ele)
+            if mod_element in clause_element:
+                remove_clauses.append(clause_element)
+                #clauses.remove(clause_element)
             else:
-                if mod_ele[0] == "~":
-                    if mod_ele[1:] in clause_ele:
-                        clause_ele.remove(mod_ele[1:])
+                if mod_element[0] == "~":
+                    if mod_element[1:] in clause_element:
+                        clause_element.remove(mod_element[1:])
                 else:
-                    if ("~" + mod_ele) in clause_ele:
-                        clause_ele.remove("~" + mod_ele)
-            if not clause_ele:
+                    if ("~" + mod_element) in clause_element:
+                        clause_element.remove("~" + mod_element)
+            if not clause_element:
                 return False
 
-    if not newClause:
+    if not clauses:
+        Answer(model)
         return True
 
-    for c in newClause:
-        if not c:
+    for clause_ele in clauses:
+        if not clause_ele:
             return False
+
     for clause in remove_clauses:
-        newClause.remove(clause)
+        clauses.remove(clause)
     return "Cont"
 
 def PureSymbol(symbols,clauses,model):
-    isPure = 0
+    pure_symbol = 0
+    remove_clauses = []
     if not symbols:
         return None
-    for clause_ele in clauses:
-        isPure = 1
-        for del_clause in clause_ele:
-            for ele in clauses:
+    for clause_element in clauses:
+        pure_symbol = 1
+        for del_clause in clause_element:
+            for element in clauses:
                 if del_clause[0] == "~":
-                    if del_clause[1:] in ele:
-                        isPure = 0
+                    if del_clause[1:] in element:
+                        pure_symbol = 0
                         break
                 else:
-                    if ("~" + del_clause) in ele:
-                        isPure = 0
+                    if ("~" + del_clause) in element:
+                        pure_symbol = 0
                         break
-            if isPure:
-                model.add(del_clause)
-                for j in clauses:
-                    if del_clause in j:
-                        clauses.remove(j)
+            if pure_symbol:
+                for items in clauses:
+                    if del_clause in items:
+                        remove_clauses.append(items)
+                for element in remove_clauses:
+                    clauses.remove(element)
                 return del_clause
     return None
 
 def UnitClause(clauses,model):
-    for clause_ele in clauses:
-        if len(clause_ele) == 1:
-            model.add(clause_ele[0])
+    remove_clauses = []
+    for clause_element in clauses:
+        if len(clause_element) == 1:
+            model.add(clause_element[0])
             for del_clause in clauses:
-                if clause_ele[0] in del_clause:
-                    clauses.remove(del_clause)
+                if clause_element[0] in del_clause:
+                    remove_clauses.append(del_clause)
                 else:
-                    if clause_ele[0][0] == "~":
-                        if clause_ele[0][1:] in del_clause:
-                            del_clause.remove(clause_ele[0][1:])
+                    if clause_element[0][0] == "~":
+                        if clause_element[0][1:] in del_clause:
+                            del_clause.remove(clause_element[0][1:])
                     else:
-                        if ("~" + clause_ele[0]) in del_clause:
-                            del_clause.remove("~" + clause_ele[0])
-            return clause_ele[0]
+                        if ("~" + clause_element[0]) in del_clause:
+                            del_clause.remove("~" + clause_element[0])
+            for item in remove_clauses:
+                clauses.remove(item)
+            return clause_element[0]
     return None
-
-def remove(symbols,P):
-    if symbols:
-        if P[0] == "~":
-            symbols.discard(P[1:])
-        else:
-            symbols.discard(P)
-    return symbols
-
-def Union(model,P):
-    model.add(P)
-    return model
 
 def Dpll(clauses,symbols,model):
 
-    if not symbols:
-        return model
+    if not clauses:
+        Answer(model)
+        return True
+
     result = ModelCheck(clauses,model)
-    print "\nIN Dpll"
-    print "clauses: ",clauses
-    print "symbols: ",symbols
-    print "model : ",model
+
     if result == True:
+        Answer(model)
         return True
     elif result == False:
         return False
@@ -161,54 +176,54 @@ def Dpll(clauses,symbols,model):
         P = PureSymbol(symbols,clauses,model)
         if P:
             model.add(str(P))
-            print "Added Pure",P
-            print 'Model now is',model
-            return Dpll(clauses, remove(symbols, P), model)
+            if symbols:
+                remove(symbols,P)
+            return Dpll(clauses, symbols, model)
         P = UnitClause(clauses,model)
         if P:
             model.add(str(P))
-            print "Added Unit",P
-            print 'Model now is', model
-            return Dpll(clauses, remove(symbols, P), model)
+            if symbols:
+                remove(symbols,P)
+            return Dpll(clauses, symbols, model)
+
+        if not symbols:
+            Answer(model)
+            return True
 
         if not clauses:
+            Answer(model)
             return True
+
         P = symbols.pop()
-        if P[0] == "~":
-            P_1 = str(P[1:])
-        else:
-            P_1 =("~"+str(P))
-        #print "Added not both of them", model, model_1
-        return Dpll(clauses,remove(symbols,P),Union(model,P)) or \
-               Dpll(clauses,remove(symbols,P),Union(model,P_1))
+        remove(symbols,P)
+
+        return Dpll(clauses,copy.deepcopy(symbols),Union(model,P,True)) or \
+               Dpll(clauses,copy.deepcopy(symbols),Union(model,P,False))
 
 def DpllSatisfiable():
-    #model = Set([])
     list_symbols = symbols
-    model = Set([])
+    model = set()
     result = Dpll(list_clauses,list_symbols,model)
     if result:
         print "yes"
-        output.write("yes")
-        print Answer(model)
+        output.write("yes\n")
     else:
-        output.write("no")
+        output.write("no\n")
         print "no"
+    print file_logs[1:]
+    output.write(file_logs[1:])
 
 guests,tables = [int(x) for x in input.readline().strip().split(" ")]
-#print guests, tables
-for l in input.readlines():
-    temp = l.strip().split(" ")
+for line in input.readlines():
+    temp = line.strip().split(" ")
     if temp[2] == 'F':
         friends.append([temp[0],temp[1]])
     else:
         enemies.append([temp[0], temp[1]])
 
-
 Cond1()
 Cond2()
 Cond3()
-#print list_clauses
 DpllSatisfiable()
 
 
